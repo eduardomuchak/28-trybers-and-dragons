3f75e727b87b742468f2ff27a33df4149aaa7f17
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-10 20:08:51 +0000

    feat: :sparkles: create pve class extended by battle class
    Teste para o requisito 11. Avaliador local não esta rodando este teste.

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,32 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+type MonstersTypes = SimpleFighter[] | Fighter[];
+
+export default class PVE extends Battle {
+  private _player: Fighter;
+  private _monsters: MonstersTypes;
+  constructor(player: Fighter, monsters: MonstersTypes) {
+    super(player);
+    this._player = player;
+    this._monsters = monsters;
+  }
+
+  fight(): number {
+    const player = this._player;
+    const monsters = this._monsters;
+    let winner = 1;
+
+    monsters.forEach((monster) => {
+      while (player.lifePoints >= 0 && monster.lifePoints >= 0) {
+        player.attack(monster);
+        monster.attack(player);
+      }
+      if (player.lifePoints === -1) {
+        winner = -1;
+      }
+    });
+
+    return winner;
+  }
+}
